aa8958799f45d73130e555ac2c795eb9655d266a
Paulo Gomes <paulo.gomes@suse.com>
2023-06-19 13:20:53 +0100

    replace docker pull
    Signed-off-by: Paulo Gomes <paulo.gomes@suse.com>

--- scripts/k3s-images.sh
+++ scripts/k3s-images.sh
@@ -8,9 +8,10 @@ mkdir -p bin
 # This is used for downloading the tar file and not the images text file.
 # Referenced in test: tests/validation/tests/v3_api/test_airgap.py
 if [ -e /usr/tmp/k3s-images.txt ]; then
-    images=$(grep -e 'docker.io/rancher/mirrored-pause' -e 'docker.io/rancher/mirrored-coredns-coredns' /usr/tmp/k3s-images.txt)
-    xargs -n1 docker pull <<< "${images}"
-    docker save -o ./bin/k3s-airgap-images.tar ${images}
+    sed 's;docker.io/;;g' /usr/tmp/k3s-images.txt > /usr/tmp/docker-k3s-images.txt
+    images=$(grep -e 'rancher/mirrored-pause' -e 'rancher/mirrored-coredns-coredns' /usr/tmp/docker-k3s-images.txt)
+    ./scripts/download-frozen-image-v2.sh /tmp/images ${images}
+    tar -C /tmp/images -cf './bin/k3s-airgap-images.tar' .
 else
     touch bin/k3s-airgap-images.tar
 fi
